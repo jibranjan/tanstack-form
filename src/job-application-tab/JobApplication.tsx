470073b70d754713e9351a328f6f7e795f42c8f8
Jibran Jan <jibranjan77@gmail.com>
2025-01-17 16:39:39 +0530

    rmeove render fields function

--- src/job-application-tab/JobApplication.tsx
+++ src/job-application-tab/JobApplication.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import OtherFields from "./OtherFields.tsx";
 
 interface JobApplicationProps {
     form: any;
@@ -22,7 +23,7 @@ function JobApplication({ form }: JobApplicationProps) {
         {
             id: 'email-invitations',
             title: 'Email invitations',
-            description: 'Each candidate is emailed a single-use URL. Helps prevent assessments from being shared.'
+            description: 'Each candidate is emailed a single-use URL.'
         }
     ];
 
@@ -34,76 +35,6 @@ function JobApplication({ form }: JobApplicationProps) {
         { id: 'notice-period', name: 'Notice period', isMandatory: false, section: 'other', allowChange: true },
         { id: 'languages-known', name: 'Languages known', isMandatory: false, section: 'other', allowChange: true },
     ]);
-
-    const renderFieldSection = (section: 'important' | 'other') => {
-        return (
-            <div className="grid grid-cols-2 gap-x-5 gap-y-2 p-2">
-                {fields.filter((fld) => fld.section === section).map((fld) => {
-                    const selectionFieldName = `${section}-field-${fld.id}-selection`;
-                    const mandatoryFieldName = `${section}-field-${fld.id}-mandatory`;
-                    return (
-                        <div key={fld.id} className="flex items-center justify-between gap-2 col-span-1">
-                            <form.Field
-                                name={selectionFieldName}
-                                defaultValue={fld.isMandatory}
-                                children={(field: any) => {
-                                    return (
-                                        <label 
-                                            htmlFor={`${fld.id}-checkbox`} 
-                                            className="text-sm text-gray-700 font-light flex items-center gap-2"
-                                        >
-                                            <input 
-                                                id={`${fld.id}-checkbox`}
-                                                className="w-3.5 h-3.5 accent-blue-900 mt-0.5 rounded-sm" 
-                                                type="checkbox" 
-                                                checked={field.state.value || form.getFieldValue(mandatoryFieldName) || false}
-                                                disabled={!fld.allowChange || form.getFieldValue(mandatoryFieldName) || false}
-                                                onBlur={field.handleBlur}
-                                                onChange={(e) => field.handleChange(e.target.checked)}
-                                            />
-                                            {fld.name}
-                                        </label>
-                                    )
-                                }}
-                            />
-                            <form.Field
-                                name={mandatoryFieldName}
-                                defaultValue={fld.isMandatory}
-                                children={(field: any) => {
-                                    return (
-                                        <label 
-                                            htmlFor={`${fld.id}-mandatory`} 
-                                            title="Mark as mandatory"
-                                            className="relative inline-flex items-center"
-                                        >
-                                            <input
-                                                id={`${fld.id}-mandatory`}
-                                                type="checkbox"
-                                                className="sr-only peer"
-                                                checked={field.state.value || false}
-                                                disabled={!fld.allowChange || false}
-                                                onBlur={field.handleBlur}
-                                                onChange={(e) => {
-                                                    field.handleChange(e.target.checked);
-                                                    // If mandatory is checked, ensure selection is also checked
-                                                    if (e.target.checked) {
-                                                        form.setFieldValue(selectionFieldName, true);
-                                                        setFields((prevFields) => prevFields.map((prevField) => prevField.id === fld.id ? { ...fld, isMandatory: true } : prevField));
-                                                    }
-                                                }}
-                                            />
-                                            <div className="cursor-pointer w-7 h-3.5 bg-gray-400 outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:absolute after:top-[1px] after:left-[2px] after:text-[10px] after:flex after:items-center after:justify-center after:text-emerald-500 after:bg-white after:rounded-full after:h-3 after:w-3 after:transition-all peer-checked:bg-emerald-500 peer-disabled:bg-gray-200"></div>
-                                        </label>
-                                    )
-                                }}
-                            />
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    };
-
 
     return (
         <section className="flex flex-col gap-5 bg-white rounded-lg px-5 py-3 shadow-md">
@@ -167,7 +98,12 @@ function JobApplication({ form }: JobApplicationProps) {
                             First name, last name and email are already added.
                         </p>
                     </h3>
-                    {renderFieldSection('other')}
+                    <OtherFields 
+                        form={form}
+                        fields={fields} 
+                        section="other"
+                        setFields={setFields}
+                    />
                 </div>
             </div>
 
